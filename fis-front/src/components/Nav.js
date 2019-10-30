6991801b6be0d1e97dc49748842be18381de14d2
sachinm78 <sachinm78@gmail.com>
2019-10-30 14:41:06 -0500

    added some basic styling to nav bar, links and page content; fixed nav links

--- fis-front/src/components/Nav.js
+++ fis-front/src/components/Nav.js
@@ -6,8 +6,8 @@ const Nav = () => {
         <div className = 'Nav-wrapper'>
             <div className = 'Nav-links-wrapper'>
                 <Link to = '/' className = 'Nav-link' activeClassName = 'Active-link'>Home</Link>
-                <Link to = '/superheroes' className = 'Nav-link' activeClassName = 'Active-link'>About</Link>
-                <Link to = '/starwars' className = 'Nav-link' activeClassName = 'Active-link'>Contact</Link>
+                <Link to = '/about' className = 'Nav-link' activeClassName = 'Active-link'>About</Link>
+                <Link to = '/contact' className = 'Nav-link' activeClassName = 'Active-link'>Contact</Link>
             </div>
         </div>
     )
