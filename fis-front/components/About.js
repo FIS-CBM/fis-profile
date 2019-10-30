0dc0abd6320667e3c7e05842cddf11f0f08ac3fb
sachinm78 <sachinm78@gmail.com>
2019-10-30 13:32:54 -0500

    created components for Nav, About and Contact with placeholder text; set up React Router in App.js

---
+++ fis-front/components/About.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const About = () => {
+    return (
+    <div class='About-wrapper'>
+        <div class='About-header'>
+            <h1>About Page</h1>
+        </div>
+        <div class='About-body'>
+            <p>Add content here</p>
+        </div>
+    </div>
+    )
+}
+
+export default About;
