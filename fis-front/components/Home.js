95df5141e2c0618c2d237172a892a7e921a60872
Sachin Mahajan <45132585+sachinm78@users.noreply.github.com>
2019-10-30 13:21:20 -0500

    #2 [Home Page] Created Home component and moved it out of App.js (#6)
    * changed background color on home page
    
    * edited home page content; what content is needed?
    
    * created Home component and moved content out of App.js

---
+++ fis-front/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Home = () => {
+    return (
+    <div class='Home-wrapper'>
+        <div class='Home-header'>
+            <h1>HOME PAGE</h1>
+        </div>
+        <div class='Home-body'>
+            <p>What content do we want on this page?</p>
+        </div>
+    </div>
+    )
+}
+
+export default Home;
--- fis-front/src/App.js
+++ fis-front/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>HOME PAGE</h1>
-        <p>What content do we want on this page?</p>
-      </header>
+
     </div>
   );
 }
